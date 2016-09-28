9767d9fac2490f68cb76a4f275db31d2a4c1b76f
jpsierens <jpsierens1988@gmail.com>
2016-09-28 08:10:35 -0600

    flow init, actions index

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,20 +1,26 @@
+// @flow
 import * as types from './types';
 
-export function addBudget(budget) {
+type addBudgetType = {
+    name: string,
+    note: string
+};
+
+export function addBudget(budget: addBudgetType) {
     return {
         type: types.ADD_BUDGET_CLICK,
         data: budget
     };
 }
 
-export function removeBudget(id) {
+export function removeBudget(id: string) {
     return {
         type: types.REMOVE_BUDGET_CLICK,
         id
     };
 }
 
-export function updateBudget(id, updates) {
+export function updateBudget(id: string, updates: Object) {
     return {
         type: types.UPDATE_BUDGET_CLICK,
         updates,
@@ -22,7 +28,16 @@ export function updateBudget(id, updates) {
     };
 }
 
-export function moveBudget(dragIndex, hoverIndex, budget) {
+type moveBudgetType = {
+    __v: number,
+    _id: string,
+    completed: boolean,
+    updatedAt: string,
+    name: string,
+    note: string
+};
+
+export function moveBudget(dragIndex: number, hoverIndex: number, budget: moveBudgetType) {
     return {
         type: types.MOVE_BUDGET,
         dragIndex,
