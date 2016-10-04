3569b5d9d2cfe6444fc3d9ad6399b1b7002eebb0
jpsierens <jpsierens1988@gmail.com>
2016-10-04 14:40:11 -0600

    componentization of BudgetDetail

--- app/components/BudgetDetail.js
+++ app/components/BudgetDetail.js
@@ -1,75 +1,22 @@
 // @flow
 import React from 'react';
 import { withRouter } from 'react-router';
-import handleUpdateItem from '../helpers/handleUpdateItem';
-import withExit from '../helpers/withExit';
 import { BudgetType } from '../types';
-import * as rules from '../rules';
-
-const handleSave = (router, nameInput, noteInput, params) => {
-    const newName = nameInput.value;
-    const newNote = noteInput.value;
-
-    if (!newName || !newNote) return;
-
-    params.push({
-        name: newName,
-        note: newNote
-    });
-
-    withExit(handleUpdateItem)(router, '/', params);
-};
+import BudgetEdit from './BudgetEdit';
 
 type Props = {
     budget: BudgetType,
     onRemove: () => void,
-    updateBudget: () => void,
     router: Object
 };
 
-const BudgetDetail = ({ budget, updateBudget, onRemove, router }: Props) => {
-    const { name, note, _id, completed, updatedAt } = budget;
-    const time = new Date(updatedAt);
-
-    let nameInput;
-    let noteInput;
+const BudgetDetail = (props: Props) => {
+    const { budget, onRemove, router } = props;
+    const { _id, completed } = budget;
 
     return (
         <div className={`budget budget-detail ${ completed ? 'done' : ''}`}>
-            <form>
-                <input
-                    type="text"
-                    defaultValue={name}
-                    maxLength={`${rules.NAME_LENGTH}`}
-                    ref={(ref) => { nameInput = ref; }}/>
-
-                <textarea
-                    rows="10"
-                    cols="50"
-                    defaultValue={note}
-                    maxLength={`${rules.NOTE_LENGTH}`}
-                    ref={(ref) => { noteInput = ref; }}/>
-            </form>
-
-            <div>
-                <button
-                    className="btn-status"
-                    onClick={(e) => handleUpdateItem(e, updateBudget, _id, {
-                        completed: !completed
-                    })}>
-
-                    Status: { completed ? 'Done' : 'Not Done'}
-                </button>
-                <span className="datetime">
-                    Last Updated: { time.toLocaleString() }
-                </span>
-                <button
-                    className="btn-save"
-                    onClick={(e) => handleSave(router, nameInput, noteInput, [e, updateBudget, _id])}>
-
-                    SAVE
-                </button>
-            </div>
+            <BudgetEdit {...props}/>
 
             <hr />
 
@@ -80,6 +27,12 @@ const BudgetDetail = ({ budget, updateBudget, onRemove, router }: Props) => {
                     <div>Some description of a bought item</div>
                     <div>$15.00</div>
                 </div>
+                <button
+                    className="btn-create"
+                    onClick={() => {}}>
+
+                    CREATE
+                </button>
             </section>
 
             <span
