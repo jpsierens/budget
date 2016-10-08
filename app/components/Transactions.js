6db9107efcc59a6cbc8d5eb278d561afd32e3bbf
jpsierens <jpsierens1988@gmail.com>
2016-10-08 14:21:25 -0600

    changed Transactions to class mode

--- app/components/Transactions.js
+++ app/components/Transactions.js
@@ -1,22 +1,45 @@
 import React from 'react';
 
-const Transactions = () => {
-    return (
-        <section className="transactions">
-            <h2>Transactions</h2>
-            <div className="transaction">
-                <div>9/30/2016</div>
-                <div>Some description of a bought item</div>
-                <div>$15.00</div>
-            </div>
-            <button
-                className="btn-create"
-                onClick={() => {}}>
+export default class Transactions extends React.Component {
+    state = {
+        create: false
+    };
 
-                CREATE
-            </button>
-        </section>
-    );
-};
+    handleCreateClick() {
+        console.log('hola');
+        this.setState({ create: true });
+    }
 
-export default Transactions;
+    handleSaveClick() {
+        console.log('adios');
+        this.setState({ create: false });
+    }
+
+    render() {
+        const btnCreate = (<button
+            className="btn-create"
+            onClick={() => { this.handleCreateClick.bind(this); }}>
+
+            CREATE
+        </button>);
+
+        const btnSave = (<button
+            className="btn-create"
+            onClick={() => { this.handleSaveClick.bind(this); }}>
+
+            SAVE
+        </button>);
+
+        return (
+            <section className="transactions">
+                <h2>Transactions</h2>
+                <div className="transaction">
+                    <div>9/30/2016</div>
+                    <div>Some description of a bought item</div>
+                    <div>$15.00</div>
+                </div>
+                { (this.state.create) ? btnSave : btnCreate }
+            </section>
+        );
+    }
+}
